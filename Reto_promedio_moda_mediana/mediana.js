cd72f422a2fc602e30c28b0538d2302b7830e644
Luis Manrique <luismanrique158158@gmail.com>
2021-08-16 18:08:25 -0500

    Agregando HTML y JS de mediana con método sin función

--- Reto_promedio_moda_mediana/mediana.js
+++ Reto_promedio_moda_mediana/mediana.js
@@ -1,17 +1,47 @@
-
-function calcularMediaAritmetica(lista){
-    // let sumaLista = 0;
-    // for (let i = 0; i < lista.length; i++) {
-    // sumaLista = sumaLista + lista[i];
-    // }
-
-    const sumaLista = lista.reduce(
+function calcularMediaAritmetica(lista1){
+    const sumaLista1 = lista1.reduce(
         function (valorAcumulado = 0, nuevoElemento){
             return valorAcumulado + nuevoElemento;
         }
     );
 
-    const promedioLista = sumaLista/lista.length;
+    const promedioLista1 = sumaLista1/lista1.length;
 
-    return promedioLista;
+    return promedioLista1;
+}
+
+const lista1 = [
+    100,
+    200,
+    500,
+    4000000,
+];
+const mitadLista1 = parseInt(lista1.length / 2);
+
+function esPar(numerito){
+    if (numerito % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let mediana;
+
+
+if (esPar(lista1.length)){
+    const elemento1 = lista1[mitadLista1- 1];
+    const elemento2 = lista1[mitadLista1];
+
+    const promedioElemento1y2 = calcularMediaAritmetica([
+        elemento1,
+        elemento2,
+    ]);
+
+    mediana = promedioElemento1y2;
+
+} else {
+    mediana = lista1[mitadLista1];
+
+    //posicion mitadlista1 dentro de Lista 1 --> mediana 
 }
