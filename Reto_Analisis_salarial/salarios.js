3ee3d6900d02a178d949e798b9961775f23e3562
Luis Manrique <luismanrique158158@gmail.com>
2021-08-19 17:52:34 -0500

    Agregando Javascript y calculando mediana salarial

---
+++ Reto_Analisis_salarial/salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push ({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push ({
+    name: "Natalia",
+    salary: 1500,
+});
+colombia.push ({
+    name: "José",
+    salary: 3000,
+});
+colombia.push ({
+    name: "Pepe",
+    salary: 3000,
+});
+colombia.push ({
+    name: "Marina",
+    salary: 5000,
+});
+colombia.push ({
+    name: "Josefa",
+    salary: 2000,
+});
+colombia.push ({
+    name: "Raimundo",
+    salary: 1500,
+});
+colombia.push ({
+    name: "Pablo",
+    salary: 800,
+});
+colombia.push ({
+    name: "Jeyson",
+    salary: 4000,
+});
+colombia.push ({
+    name: "Bill",
+    salary: 3750,
+});
+colombia.push ({
+    name: "Pedro",
+    salary: 4500,
+});
+colombia.push ({
+    name: "José",
+    salary: 3000,
+});
+colombia.push ({
+    name: "Germinio",
+    salary: 2400,
+});
+colombia.push ({
+    name: "Paco",
+    salary: 2100,
+});
+colombia.push ({
+    name: "Quitian",
+    salary: 2500,
+});
+colombia.push ({
+    name: "Paquito",
+    salary: 2400,
+});
+colombia.push ({
+    name: "Picandro",
+    salary: 3500,
+});
+colombia.push ({
+    name: "Legandra",
+    salary: 4500,
+});
+colombia.push ({
+    name: "Katherine",
+    salary: 5500,
+});
+colombia.push ({
+    name: "Luis",
+    salary: 8500,
+});
